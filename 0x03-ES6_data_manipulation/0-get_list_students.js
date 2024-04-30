6359f03c0285ee8256d5edf5f349abf7c261b3a2
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-04-30 09:01:05 +0000

    a change in task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,3 @@
-/**
- * lists of students.
- * @returns {{id: Number, firstName: String, and location: String}[]}
- */
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
