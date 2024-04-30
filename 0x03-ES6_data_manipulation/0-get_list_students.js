535dc5e36c9ece90bdb81133ed98574cf808e9be
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-04-30 08:41:59 +0000

    a function named getListStudents that returns an array of objects.
    Each object should have three attributes: id (Number), firstName (String), and location (String).
    
    The array contains the following students in order:
    
    Guillaume, id: 1, in San Francisco
    James, id: 2, in Columbia
    Serena, id: 5, in San Francisco

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
