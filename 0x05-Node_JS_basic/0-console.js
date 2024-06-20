1596e1817307390963cc8f57cd73559211a698b4
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-20 15:24:47 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * display a message on the STDOUT.
+ * param {string} msg the message to display 
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
