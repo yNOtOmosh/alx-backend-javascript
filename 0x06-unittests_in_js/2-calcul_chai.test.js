c6e8cd51fc9147a4c644f56987b164bda3d47fa2
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-20 17:04:48 +0000

    modified test file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,35 +4,35 @@ const calculateNumber = require('./2-calcul_chai.js');
 describe('calculateNumber', function() {
   describe('SUM', function() {
     it('should return 6 when inputs are 1.4 and 4.5', function() {
-      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      expect(calculateNumber('SUM', 1.4, 4.5), 6);
     });
 
     it('should return 0 when inputs are -0.4 and 0.4', function() {
-      assert.strictEqual(calculateNumber('SUM', -0.4, 0.4), 0);
+      expect(calculateNumber('SUM', -0.4, 0.4), 0);
     });
   });
 
   describe('SUBTRACT', function() {
     it('should return -4 when inputs are 1.4 and 4.5', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
 
     it('should return 1 when inputs are 0.5 and -0.5', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.5, -0.5), 1);
+      expect(calculateNumber('SUBTRACT', 0.5, -0.5), 1);
     });
   });
 
   describe('DIVIDE', function() {
     it('should return 0.2 when inputs are 1.4 and 4.5', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
     it('should return Error when divisor is 0', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 
     it('should return Error when rounded divisor is 0', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
     });
   });
 });
