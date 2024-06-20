3ea30e380cdd8af1444a6f761b9136933856638c
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-20 16:33:20 +0000

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return 0 when inputs are -0.4 and 0.4', function() {
+      assert.strictEqual(calculateNumber('SUM', -0.4, 0.4), 0);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return 1 when inputs are 0.5 and -0.5', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 0.5, -0.5), 1);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return Error when divisor is 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+
+    it('should return Error when rounded divisor is 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
+    });
+  });
+});
